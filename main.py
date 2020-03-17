e57c3d258957e62a8b4527b82c46036130990670
Huy Đỗ <huydo93.it@gmail.com>
2020-03-17 16:17:25 +0900

    test

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 value = [('acc1', 1500), ('acc2', 500), ('acc3', 750)]
 info = dict(value)
-acc = str(input("Input acc name: "))
-if acc not in info.keys():
-    print("Acc dont exits\n")
-else:
-    print("Before: ", info.get(acc))
-    num = int(input("Input money: "))
-    info[acc] += num
-    print("After: ", info.get(acc))
+while (True):
+    acc = str(input("Input acc name: "))
+    if acc == "END":
+        break
+    if acc not in info.keys():
+        print("Acc dont exits\n")
+    else:
+        print("Before: ", info.get(acc))
+        num = int(input("Input money: "))
+        info[acc] += num
+        print("After: ", info.get(acc))
 print("END!!!")
 
